1945a72d48c41626a3473cc99dbfde740c5b126f
vincent <christianvincent724@gmail.com>
2024-11-29 11:52:04 +0700

    + css

---
+++ add.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>lorem</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+    </body>
+</html>
